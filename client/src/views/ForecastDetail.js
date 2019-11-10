f060001426f02fe1855ac0e031373e90c198254e
RicardoJavierFernandez <rickyjf808@gmail.com>
2019-11-10 11:56:10 -0500

    Added different view named ForecastDetail

--- client/src/views/ForecastDetail.js
+++ client/src/views/ForecastDetail.js
@@ -5,6 +5,8 @@ import forecastModel from '../utils/forecastingModel';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
 
 class ForecastDetail extends Component {
     constructor(props) {
@@ -34,8 +36,8 @@ componentDidMount() {
     });
 }
 
-handleChange = (event) => {
-    this.setState({[event.target.name]: event.target.value});
+handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
 }
 
 // run forecast model and use output to determine the order quantity
@@ -70,7 +72,7 @@ submitAssumptions = (e) => {
             });
     }
     else {
-        this.setState({orderAmount: "Error please input valid numbers greater than zero in the input boxes!"});
+        this.setState({orderAmount: "Please enter valid number"});
     }
 }
 
@@ -92,14 +94,31 @@ submitAssumptions = (e) => {
                     </thead>
                     <tbody>
                         <tr>
-                        <td>
+                        <td>    
                             {this.state.products.length ? (
                                 <select id="products">
                                 {this.state.products.map((product, index) => 
                                     <option key={index} value={product} id={product}>Product {product}</option>
                                 )}
                                 </select>
-                            ) : (<strong>No Products</strong>)}
+                            ) : (<strong>No options</strong>)}
+                            
+                            {/*<DropdownButton
+                                size="sm"
+                                variant="secondary"
+                                title="Select Product"
+                                id={`dropdown-button-drop-${1}`}
+                                key={1}
+                            >
+                            {this.state.products.length ? (
+                                <select id="products">
+                                this.state.products.map((product, index) => 
+                                    <Dropdown.Item key={index} eventKey={product} id={product}>Product {product}</Dropdown.Item>
+                                    <option key={index} value={product} id={product}>Product {product}</option>
+                                )
+                                </select>
+                            ) : (<strong>No Products</strong>)
+                            </DropdownButton>*/}
                         </td>
                         <td>
                             <input type="text" name="unitsOrder"placeholder="Units Ordered" onChange={this.handleChange}></input>
@@ -117,10 +136,10 @@ submitAssumptions = (e) => {
                             <input type="text" name="leadTime" placeholder="Lead Time" onChange={this.handleChange}></input>
                         </td>
                         {this.state.orderAmount > 0 ?
-                        <td  className="alert-success" onClick={() => alert('Order now!')}>    
-                            <strong >{this.state.orderAmount}</strong> 
+                        <td  className="alert-info" onClick={() => alert('Order now!')}>    
+                            <strong>{this.state.orderAmount}</strong> 
                         </td>
-                        : <td><strong></strong></td>}
+                        : <td><strong>{this.state.orderAmount}</strong></td>}
                         </tr>
                     </tbody>
                 </Table>
