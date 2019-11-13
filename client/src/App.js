42aaca0e077ca133d406595e9086b72e6378898e
RicardoJavierFernandez <rickyjf808@gmail.com>
2019-11-12 21:37:00 -0500

    Fixed registration form, and incoporated react bootstrap for it. Built user api route, and modified the user model to only have name. Also updated API.js to include register and login functions.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import Nav from 'react-bootstrap/Nav';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import ForecastDetail from './views/ForecastDetail';
+// import ForecastDetail from './views/ForecastDetail';
 import CreateOrder from './views/CreateOrder';
+import RegistrationForm from './views/RegistrationForm';
 
 function App() {
   return (
     <Router>
       <div>
-        <CreateOrder />
+        <RegistrationForm />
       </div>
     </Router>
   );
