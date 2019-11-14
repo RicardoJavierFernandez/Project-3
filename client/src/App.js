dac6ef1f938e7d836e9fb44f93905b0fc5d9c7f7
Alan Tsoi <alanstephentsoy@gmail.com>
2019-11-13 21:17:17 -0500

    dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import Nav from 'react-bootstrap/Nav';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import ForecastDetail from './views/ForecastDetail';
+// import Login from '../../client/src/containers/LoginPage';
+// import Homepage from '../../client/src/containers/HomePage';
 import CreateOrder from './views/CreateOrder';
+import Registration from '../../client/src/containers/Registration';
+import ForecastDetail from './views/ForecastDetail';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <CreateOrder />
-      </div>
-    </Router>
-  );
+
+class App extends Component {
+  constructor (props)
+  {
+      super(props);
+      this.state = {
+          session:null
+      }
+  }
+  signIn = (session) => this.setState({session});
+
+  render () {
+    return (
+        <Router>
+        <Switch>
+        {/* <Route path ="/" component = {CreateOrder} /> */}
+            {/* {!this.state.session && <Route path ="/login" component = {()=> <Login onlogin= {this.signIn}/>} />} */}
+        {!this.state.session && <Route path ="/register" component = {()=> <Registration onRegister= {this.signIn}/>} />}
+        <Route path="/createorders" component = {() => <CreateOrder session= {this.state.session} />}/>
+        <Route path="/createorders" component = {() => <CreateOrder session= {this.state.session} />}/>
+        <Route path="/forecast" component={() => <ForecastDetail session={this.state.session} />} />
+        </Switch>
+        </Router>
+    )
+}
 }
 
 export default App;
---
+++ client/src/Components/CustomButton/CustomButton.jsx
@@ -0,0 +1,47 @@
+/*!
+
+=========================================================
+* Light Bootstrap Dashboard React - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { Component } from "./node_modules/react";
+import { Button } from "./node_modules/react-bootstrap";
+import cx from "./node_modules/classnames";
+import PropTypes from "./node_modules/prop-types";
+
+class CustomButton extends Component {
+  render() {
+    const { fill, simple, pullRight, round, block, ...rest } = this.props;
+
+    const btnClasses = cx({
+      "btn-fill": fill,
+      "btn-simple": simple,
+      "pull-right": pullRight,
+      "btn-block": block,
+      "btn-round": round
+    });
+
+    return <Button className={btnClasses} {...rest} />;
+  }
+}
+
+CustomButton.propTypes = {
+  fill: PropTypes.bool,
+  simple: PropTypes.bool,
+  pullRight: PropTypes.bool,
+  block: PropTypes.bool,
+  round: PropTypes.bool
+};
+
+export default CustomButton;
